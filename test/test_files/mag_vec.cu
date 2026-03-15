f63aed55d9ae08224a2dba25ae2d5a8f813ff2b4
Mahad Siddiqui <mahadsiddiqui07@outlook.com>
2026-03-15 12:42:52 +0000

    finished mag vec test

--- test/test_files/mag_vec.cu
+++ test/test_files/mag_vec.cu
@@ -1,4 +1,59 @@
+#include "vec.h"
+#include "../test_runner.h"
+#include <cmath>
+#include <cassert>
+
+template<size_t dim>
+__global__ void mag_vec_kernel(const vec<dim> *v1, float *mag) {
+	*mag = v1->GetMagnitude();
+}
+
+template<size_t dim>
+void mag_vec_cu() {
+	const float epsilon = 2e-6;
+
+	vec<dim> *v1;
+	float *mag;
+
+	cudaMallocManaged(&v1, sizeof(vec<dim>));
+	cudaMallocManaged(&mag, sizeof(float));
+
+	*v1 = init_vec<dim>();
+
+	mag_vec_kernel<<<1, 1>>>(v1, mag);
+	cudaDeviceSynchronize();
+	
+	float sum = 0;
+	for (size_t i = 0; i < dim; i++) sum += v1->data[i] * v1->data[i];
+	sum = std::sqrt(sum);
+
+	assert(sum - epsilon <= *mag && sum + epsilon >= *mag);
+
+	cudaFree(v1);
+	cudaFree(mag);
+}
+
+template<size_t dim>
+void mag_vec_cpp() {
+	const vec<dim> v1 = init_vec<dim>();
+	float mag = v1.GetMagnitude();
+
+	float sum = 0;
+	for (size_t i = 0; i < dim; i++) sum += v1.data[i] * v1.data[i];
+	sum = std::sqrt(sum);
+
+	assert(sum == mag);
+}
+
+template<size_t dim>
+struct mag_vec {
+	void operator()() {
+		mag_vec_cpp<dim>();
+		mag_vec_cu<dim>();
+	}
+};
+
 int main() {
-	return 0;
+	run_tests<mag_vec, 2, 64>();
 }
 
